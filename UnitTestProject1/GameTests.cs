c966531900c7a1ad359cd0fd0f8e7720f03bdeb4
decho <dechomecho@gmail.com>
2015-08-23 17:12:27 +0300

    Renamed ingra_baloni.csproj. Created a unit test project.

---
+++ UnitTestProject1/GameTests.cs
@@ -0,0 +1,14 @@
+﻿namespace BaloonsPopTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
