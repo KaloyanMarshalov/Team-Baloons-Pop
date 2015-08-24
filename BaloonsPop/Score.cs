f18695981a7c3b1a20cd9aceb01b5a4f8f912e40
dechoD <dechomecho@gmail.com>
2015-08-24 19:04:18 +0300

    Implemented a Score class.

---
+++ BaloonsPop/Score.cs
@@ -0,0 +1,56 @@
+﻿namespace PoppingBaloons
+{
+    using System;
+
+    class Score
+    {
+        private const int MinimumPlayerNameLength = 3;
+        private string nameOfPlayer;
+        private int score;
+
+        public Score(string nameOfPlayer, int score)
+        {
+            this.nameOfPlayer = nameOfPlayer;
+            this.score = score;
+        }
+
+        public string NameOfPlayer
+        {
+            get
+            {
+                return this.nameOfPlayer;
+            }
+            set
+            {
+                if (value.Length <= MinimumPlayerNameLength)
+                {
+                    throw new ArgumentOutOfRangeException("Player name must be longer than " + (MinimumPlayerNameLength - 1) + " symbols");
+                }
+
+                this.nameOfPlayer = value;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return this.score;
+            }
+            set
+            {
+                if (value < 0)
+                {
+		            throw new ArgumentOutOfRangeException("Score must be zero or more");
+                }
+
+                this.score = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.NameOfPlayer + " " + this.Score;
+        }
+    }
+}
