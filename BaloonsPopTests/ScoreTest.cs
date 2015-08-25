d0458dd71370ac1636d30d6a6c8bd6f6b0367fe2
decho <dechomecho@gmail.com>
2015-08-25 13:36:32 +0300

    Added Score state validation tests

--- BaloonsPopTests/ScoreTest.cs
+++ BaloonsPopTests/ScoreTest.cs
@@ -10,9 +10,32 @@
     {
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void ExpectScoreConstructorToThrowOnInvalidPointsParameterer()
+        public void ExpectScoreToThrowWhenInitializedWithInvalidPoints()
         {
             Score score = new Score("Player", -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ExpectScoreToThrowWhenInitializedWithLongName()
+        {
+            Score score = new Score("Playerwithverylongname", 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ExpectScoreToThrowWhenInitializedWithShortName()
+        {
+            Score score = new Score("Pl", 10);
+        }
+
+        [TestMethod]
+        public void ExpectScoreNotToThrowWhenInitializedWithValidParameters()
+        {
+            Score score = new Score("Player", 10);
+            string playerName = score.PlayerName;
+
+            Assert.AreEqual(playerName, "Player");
         }
     }
 }
