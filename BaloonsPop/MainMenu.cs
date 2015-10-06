7e6d946372811a103752149124924c736b1fc84c
Maskln <lyubomir.nanev@gmail.com>
2015-10-06 16:05:51 +0300

    Modified the Main Page in MainMenu.cs

--- BaloonsPop/MainMenu.cs
+++ BaloonsPop/MainMenu.cs
@@ -15,9 +15,19 @@
             logger.Log("| _ \\ |  _  || |__ | |__ | |_| || |_| || |  \\  |   |  _ _|| |_| ||  _ _| ___  |");
             logger.Log("|____||_| |_||____||____||_____||_____||_|   |_|   |_|    |_____||_|    |_____|  ");
             logger.Log("");
-            logger.Log("Welcome to “Balloons Pops” game. Please try to pop the balloons.");
-            logger.Log("Use 'top' to view the top scoreboard, 'restart' to start a new game and 'exit' to quit the game.");
+            logger.Log("                    Welcome to “Balloons Pops” game :)");
+            logger.Log("");  
+            logger.Log("                     Please try to pop the balloons!");
+            logger.Log("");   
+            logger.Log("                    Press \"Enter\" to start the game!");
+            Console.ReadLine();
+            Console.Clear();
 
+            logger.Log("* Use 'top' to view the top scoreboard!");
+            logger.Log("* Use 'restart' to start a new game!");
+            logger.Log("* Use 'exit' to quit the game!");
+            logger.Log("");
+            
             Game game = new Game(logger);
 
             while (true)
