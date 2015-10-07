ae23130a4a10206c4d14a3a2fdc5f50ca9a8dcbd
zdzdz <zdzdz.link@yahoo.com>
2015-10-07 22:28:12 +0300

    Added XML Documentation for HighScore class.

--- BaloonsPop/HighScore.cs
+++ BaloonsPop/HighScore.cs
@@ -1,21 +1,53 @@
-﻿namespace PoppingBaloons
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HighScore.cs" company="Team-Baloon-Pop">
+//   Team-Baloon-Pop
+// </copyright>
+// <summary>
+//   A class that watches for a change in state of the baloons.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PoppingBaloons
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
 
+    /// <summary>
+    /// Sealed class used for logging various messages on the console:
+    /// <list type="bullet">
+    /// <item> 
+    /// <description><see cref="HighScore()"/></description> 
+    /// </item>
+    /// <item> 
+    /// <description><see cref="AddScore"/></description> 
+    /// </item>
+    /// <item> 
+    /// <description><see cref="ToString"/></description> 
+    /// </item>
+    /// </list> 
+    /// </summary>
     public class HighScore
     {
         private const int MaxScoreCount = 5;
         private readonly SortedList<int, Score> scoreList;
         private readonly IComparer<int> descendingComparer = new ScoreComparer();
 
+        /// <summary>
+        /// A constructor used to instantiate the class.
+        /// </summary>
         public HighScore()
         {
             this.scoreList = new SortedList<int, Score>(this.descendingComparer);
         }
 
+        /// <summary>
+        /// A method used for adding scores to a scorelist.
+        /// </summary>
+        /// <param name="scoreToAdd">The parameter the method is called upon.</param>
+        /// <exception cref="System.ArgumentNullException">An exception is thrown if 
+        /// there is no score to add.</exception>
         public void AddScore(Score scoreToAdd)
         {
             if (scoreToAdd == null)
@@ -31,6 +63,10 @@
             }
         }
 
+        /// <summary>
+        /// A method used for creating messages announcing the top players.
+        /// </summary>
+        /// <returns>The method returns a new string.</returns>
         public override string ToString()
         {
             if (this.scoreList.Count == 0)
