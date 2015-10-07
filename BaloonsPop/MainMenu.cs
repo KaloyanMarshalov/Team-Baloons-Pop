7b7d90d38f8ee2f0391ff70895ff0a37d852c760
Maskln <lyubomir.nanev@gmail.com>
2015-10-07 23:30:10 +0300

    MainMenu.cs optimization

--- BaloonsPop/MainMenu.cs
+++ BaloonsPop/MainMenu.cs
@@ -1,100 +1,92 @@
 ﻿namespace PoppingBaloons
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using PoppingBaloons.Interfaces;
 
     public class MainMenu
     {
         public static void Main()
-        {                
+        {
             StartMenu();
         }
 
         public static int StartMenu() // 1 - Start Game , 2 - Quit Game
         {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            ILogger logger = ConsoleLogger.LoggerInstance;
-            logger.Log(" ____  _____  _     _     _____  _____  _     _     _____  _____  _____  _____ ");
-            logger.Log("| _  ||  _  || |   | |   |  _  ||  _  ||  \\  | |   |  _  ||  _  ||  _  ||  ___| ");
-            logger.Log("|   / | |_| || |   | |   | | | || | | || |\\ \\| |   | |_| || | | || |_| || |___ ");
-            logger.Log("| _ \\ |  _  || |__ | |__ | |_| || |_| || |  \\  |   |  _ _|| |_| ||  _ _| ___  |");
-            logger.Log("|____||_| |_||____||____||_____||_____||_|   |_|   |_|    |_____||_|    |_____|  ");
-            logger.Log(string.Empty);
-            logger.Log("                        Welcome to “Balloons Pops” game :)");
-            logger.Log(string.Empty);
-            logger.Log("                         Please try to pop the balloons!");
-            logger.Log(string.Empty);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("___________________________________________________________________________________");
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("                                   > Start Game <");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("                                     Quit Game");
+            string logo = "     ____  _____  _     _     _____  _____  _     _     _____  _____  _____  _____\n " +
+                          "   | _  ||  _  || |   | |   |  _  ||  _  ||  \\  | |   |  _  ||  _  ||  _  ||  ___|\n " +
+                          "   |   / | |_| || |   | |   | | | || | | || |\\ \\| |   | |_| || | | || |_| || |___ \n" +
+                         "    | _ \\ |  _  || |__ | |__ | |_| || |_| || |  \\  |   |  _ _|| |_| ||  _ _| ___  |\n" +
+                         "    |____||_| |_||____||____||_____||_____||_|   |_|   |_|    |_____||_|    |_____|  \n\n" +
+                         "                           Welcome to “Balloons Pops” game :)\n\n" +
+                         "                            Please try to pop the balloons!\n" +
+                         "__________________________________________________________________________________________\n";
 
+            ILogger logger = ConsoleLogger.LoggerInstance;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            logger.Log(logo);
+            Console.ForegroundColor = ConsoleColor.Red;
+            logger.Log("                                   > START GAME <\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            logger.Log("                                     QUIT GAME");
             ConsoleKeyInfo keyInfo;
             keyInfo = Console.ReadKey(true);
+
             if (keyInfo.Key == ConsoleKey.Enter)
             {
                 // Start Game
                 Console.Clear();
-                logger.Log("List of Commands");
-                logger.Log("---------------------------------------");
-                logger.Log("* Use 'top' to view the top scoreboard!");
-                logger.Log("* Use 'restart' to start a new game!");
-                logger.Log("* Use 'exit' to quit the game!");
-                logger.Log(string.Empty);
+
+                var listOfCommands = new List<string>();
+
+                listOfCommands.Add("List of Commands");
+                listOfCommands.Add("---------------------------------------");
+                listOfCommands.Add("* Use 'top' to view the top scoreboard!");
+                listOfCommands.Add("* Use 'restart' to start a new game!");
+                listOfCommands.Add("* Use 'exit' to quit the game!\n");
+
+                foreach (var item in listOfCommands)
+                {
+                    logger.Log(item);
+                }
 
                 Game game = new Game(logger);
 
                 while (true)
                 {
                     game.ParseCommand(Console.ReadLine());
-                }                      
+                }
             }
             else
             {
                 if (keyInfo.Key == ConsoleKey.DownArrow || keyInfo.Key == ConsoleKey.UpArrow)
                 {
                     Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Cyan;                    
-                    logger.Log(" ____  _____  _     _     _____  _____  _     _     _____  _____  _____  _____ ");
-                    logger.Log("| _  ||  _  || |   | |   |  _  ||  _  ||  \\  | |   |  _  ||  _  ||  _  ||  ___| ");
-                    logger.Log("|   / | |_| || |   | |   | | | || | | || |\\ \\| |   | |_| || | | || |_| || |___ ");
-                    logger.Log("| _ \\ |  _  || |__ | |__ | |_| || |_| || |  \\  |   |  _ _|| |_| ||  _ _| ___  |");
-                    logger.Log("|____||_| |_||____||____||_____||_____||_|   |_|   |_|    |_____||_|    |_____|  ");
-                    logger.Log(string.Empty);
-                    logger.Log("                        Welcome to “Balloons Pops” game :)");
-                    logger.Log(string.Empty);
-                    logger.Log("                         Please try to pop the balloons!");
-                    logger.Log(string.Empty);
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    logger.Log(logo);
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("___________________________________________________________________________________");
-                    Console.WriteLine();
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("                                     Start Game ");
+                    logger.Log("                                     START GAME\n");
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("                                   > Quit Game <");
-                                               
+                    logger.Log("                                   > QUIT GAME <");
+
                     if (Console.ReadKey(true).Key == ConsoleKey.Enter)
                     {
                         // Quit Game
                         logger.Log("Thanks for playing!!!");
-                        Environment.Exit(0);                          
-                    }                    
+                        Environment.Exit(0);
+                    }
                     else
                     {
                         if (Console.ReadKey(true).Key == ConsoleKey.UpArrow || Console.ReadKey(true).Key == ConsoleKey.DownArrow)
                         {
                             Console.Clear();
-                            return StartMenu(); // Recursion :)
                         }
                     }
                 }
             }
 
-            return 2;
+            return StartMenu();
         }
     }
 }
