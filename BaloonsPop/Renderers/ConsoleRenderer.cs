4c8106708c2b6fb6e1d534b0b8eb729ff6ae106a
Decho <dechomecho@gmail.com>
2015-10-12 07:47:45 +0300

    Partial fix to the bug causing the board to draw in red the first time. Now only score is drawn in red, lets call it a feature.

--- BaloonsPop/Renderers/ConsoleRenderer.cs
+++ BaloonsPop/Renderers/ConsoleRenderer.cs
@@ -95,6 +95,7 @@ namespace PoppingBaloons.Renderers
         /// <param name="gameboard">The instance of the Gameboard the method is called upon.</param>
         public void RenderGameboard(Gameboard gameboard)
         {
+            Console.ResetColor();
             Console.WriteLine("    0 1 2 3 4 5 6 7 8 9");
             Console.WriteLine("    --------------------");
             for (int i = 0; i < gameboard.BoardHeight; i++)
